f504d7cb896cceb6d5bb10dcf7094f0c9763b203
nirvanajoshi <damodarj36@gmail.com>
2026-09-05 21:31:28 +0545

    admin.py of enrollements

--- enrollments/admin.py
+++ enrollments/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Enrollment
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "course",
+        "status",
+        "enrolled_at",
+        "completed_at",
+    )
+
+    list_filter = (
+        "status",
+        "enrolled_at",
+    )
+
+    search_fields = (
+        "student__username",
+        "student__email",
+        "course__title",
+    )
+
+    readonly_fields = (
+        "enrolled_at",
+    )
