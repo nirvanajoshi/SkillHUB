b5bd8c1599cbe9e21219768b84fe683395b125ac
nirvanajoshi <damodarj36@gmail.com>
2026-09-05 21:36:34 +0545

    admin.py of learning

--- learning/admin.py
+++ learning/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LessonProgress
+
+
+@admin.register(LessonProgress)
+class LessonProgressAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "lesson",
+        "is_completed",
+        "watched_seconds",
+        "last_accessed_at",
+    )
+
+    list_filter = (
+        "is_completed",
+        "last_accessed_at",
+    )
+
+    search_fields = (
+        "student__username",
+        "student__email",
+        "lesson__title",
+        "lesson__chapter__course__title",
+    )
+
+    readonly_fields = (
+        "last_accessed_at",
+    )
