ad2c1a36ca52fd6f20cd568855e35e71287d8dde
nirvanajoshi <damodarj36@gmail.com>
2026-09-05 21:28:03 +0545

    admin.py of courses

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Course, Chapter, Lesson
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "instructor",
+        "category",
+        "level",
+        "is_published",
+        "created_at",
+    )
+
+    list_filter = (
+        "level",
+        "is_published",
+        "category",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+        "instructor__username",
+    )
+
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "course",
+        "chapter_number",
+    )
+
+    list_filter = ("course",)
+
+    search_fields = (
+        "title",
+        "course__title",
+    )
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "chapter",
+        "lesson_number",
+        "is_preview",
+    )
+
+    list_filter = (
+        "is_preview",
+        "chapter__course",
+    )
+
+    search_fields = (
+        "title",
+        "chapter__title",
+    )
