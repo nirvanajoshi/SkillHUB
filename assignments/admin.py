5d6368e6677d4774ce426cf7885b4079059431ef
nirvanajoshi <damodarj36@gmail.com>
2026-09-06 21:11:15 +0545

    admin.py of assigmnents

--- assignments/admin.py
+++ assignments/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Assignment, Submission
+
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "lesson",
+        "due_date",
+        "max_marks",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "due_date",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+        "lesson__title",
+        "lesson__chapter__course__title",
+    )
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "assignment",
+        "status",
+        "marks",
+        "submitted_at",
+        "graded_at",
+    )
+
+    list_filter = (
+        "status",
+        "submitted_at",
+        "graded_at",
+    )
+
+    search_fields = (
+        "student__username",
+        "student__email",
+        "assignment__title",
+    )
+
+    readonly_fields = (
+        "submitted_at",
+    )
