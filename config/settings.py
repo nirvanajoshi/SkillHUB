ea83eb3cbeb2f8a59ee96c72fba85d180c4bb4a1
nirvanajoshi <damodarj36@gmail.com>
2026-09-08 17:00:51 +0545

    account frontend

--- config/settings.py
+++ config/settings.py
@@ -209,3 +209,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ============================================================
 
 AUTH_USER_MODEL = "accounts.User"
+
+LOGIN_URL = "accounts:login"
+
+LOGIN_REDIRECT_URL = "dashboard:home"
+
+LOGOUT_REDIRECT_URL = "dashboard:home"
