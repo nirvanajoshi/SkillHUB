b5230b1609e29660ac42fd6bcd1f45f23b5a01a5
nirvanajoshi <damodarj36@gmail.com>
2026-09-09 19:21:31 +0545

    logic and views of courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Course
+
+
+def course_list(request):
+    courses = Course.objects.filter(
+        is_published=True
+    ).select_related(
+        "category",
+        "instructor",
+    )
+
+    return render(
+        request,
+        "courses/course_list.html",
+        {"courses": courses},
+    )
+
+
+def course_detail(request, slug):
+    course = get_object_or_404(
+        Course.objects.select_related(
+            "category",
+            "instructor",
+        ).prefetch_related(
+            "chapters__lessons"
+        ),
+        slug=slug,
+        is_published=True,
+    )
+
+    return render(
+        request,
+        "courses/course_detail.html",
+        {"course": course},
+    )
