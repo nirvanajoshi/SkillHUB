648ab6df1404eb979342c4e6c18591b5b7f04912
nirvanajoshi <damodarj36@gmail.com>
2026-09-10 22:28:05 +0545

    views and logic of the enrollments

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from courses.models import Course
+from .models import Enrollment
+
+
+@login_required
+def enroll_course(request, course_id):
+    course = get_object_or_404(
+        Course,
+        id=course_id,
+        is_published=True,
+    )
+
+    if request.method == "POST":
+
+        enrollment, created = Enrollment.objects.get_or_create(
+            student=request.user,
+            course=course,
+        )
+
+        if created:
+            messages.success(
+                request,
+                f"You have successfully enrolled in {course.title}.",
+            )
+        else:
+            messages.info(
+                request,
+                "You are already enrolled in this course.",
+            )
+
+        return redirect(
+            "courses:course_detail",
+            slug=course.slug,
+        )
+
+    return redirect(
+        "courses:course_detail",
+        slug=course.slug,
+    )
+
+
+@login_required
+def my_courses(request):
+    enrollments = Enrollment.objects.filter(
+        student=request.user,
+        status=Enrollment.Status.ACTIVE,
+    ).select_related(
+        "course",
+        "course__instructor",
+    )
+
+    return render(
+        request,
+        "enrollments/my_courses.html",
+        {"enrollments": enrollments},
+    )
